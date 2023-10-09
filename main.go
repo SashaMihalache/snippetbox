245e35ade0b17ef909b6ec5da880f8c3c5d4aefc
Sasha Mihalache <sasha.mihalache@gmail.com>
2023-10-09 21:24:35 +0300

    handle query id

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
+	// fmt.Println(r.URL.Path, r.URL.Query().Get("sad"))
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -17,10 +18,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Println(id)
+
+	// w.Write([]byte("Display a specific snippet with ID %d..."))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Alloed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
