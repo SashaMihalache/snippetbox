870c9f519f901e964be018f67552b0f7a52ca674
Sasha Mihalache <sasha.mihalache@gmail.com>
2023-10-17 20:43:48 +0300

    add a config struct for initial vars

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,22 +6,30 @@ import (
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP Port")
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP Network Port")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 
 	flag.Parse()
 
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	log.Printf("Starting server on %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 
 	log.Fatal(err)
 }
