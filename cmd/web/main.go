ec6d58b5ddc62de42c3309dc2362d1493ab96500
Sasha Mihalache <sasha.mihalache@gmail.com>
2024-01-28 21:14:33 +0200

    add tls certificate

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -61,6 +61,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog:       errorLog,
@@ -78,8 +79,8 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.addr)
-	err = srv.ListenAndServe()
 
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
